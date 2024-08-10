05ef6875c20d578e34424426add55d3b6e763528
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:41 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/Candle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TraderTools.Basics
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Candle
+    {
+        public long OpenTimeTicks { get; set; }
+        public long CloseTimeTicks { get; set; }
+        public float OpenBid { get; set; }
+        public float CloseBid { get; set; }
+        public float HighBid { get; set; }
+        public float LowBid { get; set; }
+        public float OpenAsk { get; set; }
+        public float CloseAsk { get; set; }
+        public float HighAsk { get; set; }
+        public float LowAsk { get; set; }
+        public float Volume { get; set; }
+        public byte IsComplete { get; set; }
+
+        public override string ToString()
+        {
+            return $"OpenTime: {new DateTime(OpenTimeTicks)} CloseTime: {new DateTime(CloseTimeTicks)} Open bid: {OpenBid} Close bid: {CloseBid} Volume: {Volume}";
+        }
+    }
+}
