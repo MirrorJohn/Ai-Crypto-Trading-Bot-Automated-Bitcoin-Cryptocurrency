6bab2d5b7780b29e5e6a02914a81658ff040a4ae
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:19:19 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Indicators/ExponentialMovingAverage.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using TraderTools.Basics;
+
+namespace TraderTools.Indicators
+{
+    public class ExponentialMovingAverage : LengthIndicator
+    {
+        private float _prevFinalValue;
+        private float _multiplier = 1;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExponentialMovingAverage"/>.
+        /// </summary>
+        public ExponentialMovingAverage()
+            : this(32)
+        {
+        }
+        
+        public ExponentialMovingAverage(int length)
+        {
+            Length = length;
+        }
+
+        /// <summary>
+        /// To reset the indicator status to initial. The method is called each time when initial settings are changed (for example, the length of period).
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            _multiplier = 2.0F / (Length + 1);
+            _prevFinalValue = 0;
+        }
+
+        public override string Name => $"EMA{Length}";
+
+        public override SignalAndValue Process(Candle candle)
+        {
+            var newValue = candle.CloseBid;
+
+            if (!IsFormed)
+            {
+                if (candle.IsComplete == 1)
+                {
+                    Buffer.Add(newValue);
+
+                    _prevFinalValue = Buffer.Sum() / Length;
+
+                    return new SignalAndValue(_prevFinalValue, IsFormed);
+                }
+
+                return new SignalAndValue((Buffer.Skip(1).Sum() + newValue) / Length, IsFormed);
+            }
+            else
+            {
+                var curValue = (newValue - _prevFinalValue) * _multiplier + _prevFinalValue;
+
+                if (candle.IsComplete == 1) _prevFinalValue = curValue;
+
+                return new SignalAndValue(curValue, IsFormed);
+            }
+        }
+    }
+}
