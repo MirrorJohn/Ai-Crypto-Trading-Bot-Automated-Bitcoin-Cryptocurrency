2b58db7aa8e41fb5f3600238097ca1de24186d66
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:46 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/DatePrice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TraderTools.Basics
+{
+    public class DatePrice
+    {
+        public DatePrice(string id, DateTime date, decimal? price)
+        {
+            Id = id;
+            Date = date;
+            Price = price;
+        }
+
+        public DatePrice(DateTime date, decimal? price)
+        {
+            Date = date;
+            Price = price;
+        }
+
+        public DatePrice()
+        {
+        }
+
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Date} {Price}";
+        }
+    }
+}
