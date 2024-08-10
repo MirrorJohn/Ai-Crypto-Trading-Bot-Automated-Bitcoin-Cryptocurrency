ad5e302f8a84f7e3c8c13e5215a592d2c64838f3
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:15:01 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/ITradeDetailsAutoCalculatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TraderTools.Basics
+{
+    [Flags]
+    public enum CalculateOptions
+    {
+        Default = 0,
+        ExcludePipsCalculations = 1,
+        IncludeOpenTradesInRMultipleCalculation = 2
+    }
+
+    public interface ITradeDetailsAutoCalculatorService
+    {
+        void RemoveTrade(Trade trade);
+        void RecalculateTrade(Trade trade);
+    }
+}
