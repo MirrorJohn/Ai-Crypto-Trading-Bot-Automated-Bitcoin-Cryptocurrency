1c4a5c68e864283e7c5dcfd92abb04d87e8187be
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:18:56 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Core.UI/Views/ChartView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TraderTools.Core.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for ChartView.xaml
+    /// </summary>
+    public partial class ChartView : UserControl
+    {
+        public ChartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
