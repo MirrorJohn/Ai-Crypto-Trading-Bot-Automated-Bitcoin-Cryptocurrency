cc5d0c2d49db6cd3946df75e5bc74d0068e82536
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:18:02 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Core.UI/Controls/SellMarkerAnnotation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using SciChart.Charting.Visuals.Annotations;
+
+namespace TraderTools.Core.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for SellMarkerAnnotation.xaml
+    /// </summary>
+    public partial class SellMarkerAnnotation : CustomAnnotation
+    {
+        public SellMarkerAnnotation()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty StrokeBrushProperty = DependencyProperty.Register(
+            "StrokeBrush", typeof(Brush), typeof(SellMarkerAnnotation), new PropertyMetadata(Brushes.Red));
+
+        public Brush StrokeBrush
+        {
+            get { return (Brush)GetValue(StrokeBrushProperty); }
+            set { SetValue(StrokeBrushProperty, value); }
+        }
+    }
+}
