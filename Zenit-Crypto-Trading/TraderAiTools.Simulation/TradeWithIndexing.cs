acd97bec1a283ceacb36248195de084f2138cae6
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:20:27 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Simulation/TradeWithIndexing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TraderTools.Basics;
+
+namespace TraderTools.Simulation
+{
+    public interface IReadOnlyTrade
+    {
+        public string Market { get; }
+        public DateTime? CloseDateTime { get; }
+    }
+
+    public class TradeWithIndexing : IReadOnlyTrade
+    {
+        public Trade Trade { get; set; }
+        public int LimitIndex { get; set; } = -1;
+        public int StopIndex { get; set; } = -1;
+        public int OrderIndex { get; set; } = -1;
+        public TradeWithIndexing Next { get; set; }
+        public TradeWithIndexing Prev { get; set; }
+
+        public string Market => Trade.Market;
+        public DateTime? CloseDateTime => Trade.CloseDateTime;
+    }
+}
