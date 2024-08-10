e42c69eb7e1aa17309b30a911de8dee6f0bd394a
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:54 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/IBrokersService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TraderTools.Basics
+{
+    public interface IBrokersService
+    {
+        void AddBrokers(IEnumerable<IBroker> brokers);
+
+        void Connect();
+
+        void LoadBrokerAccounts(ITradeDetailsAutoCalculatorService tradeCalculatorService, string mainDirectoryWithApplicationName);
+
+        IBroker GetBroker(string broker);
+
+        List<IBroker> Brokers { get; }
+
+        Dictionary<IBroker, IBrokerAccount> AccountsLookup { get; }
+    }
+}
