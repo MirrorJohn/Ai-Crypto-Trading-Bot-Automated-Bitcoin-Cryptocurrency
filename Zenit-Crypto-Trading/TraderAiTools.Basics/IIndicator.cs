6396b0045235fe3346ce2e50567062f0530db70e
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:58 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/IIndicator.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+
+namespace TraderTools.Basics
+{
+    public interface IIndicator
+    {
+        bool IsFormed { get; }
+
+        string Name { get; }
+
+        SignalAndValue Process(Candle candle);
+
+        void Reset();
+    }
+}
