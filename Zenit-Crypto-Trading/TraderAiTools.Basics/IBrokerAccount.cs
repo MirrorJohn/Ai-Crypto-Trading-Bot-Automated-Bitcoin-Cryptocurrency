9f9cb902cd69ad6cfdbc9061ffc7a3c2877fedbe
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:51 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/IBrokerAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Hallupa.TraderTools.Basics;
+
+namespace TraderTools.Basics
+{
+    public enum UpdateOption
+    {
+        OnlyIfNotRecentlyUpdated,
+        ForceUpdate
+    }
+
+    public interface IBrokerAccount
+    {
+        List<Trade> Trades { get; set; }
+
+        DateTime? AccountLastUpdated { get; set; }
+
+        string CustomJson { get; set; }
+
+        List<DepositWithdrawal> DepositsWithdrawals { get; set; }
+
+        void SaveAccount(string mainDirectoryWithApplicationName);
+
+        void UpdateBrokerAccount(
+            IBroker broker,
+            IBrokersCandlesService candleService,
+            IMarketDetailsService marketsService,
+            ITradeDetailsAutoCalculatorService tradeCalculateService,
+            UpdateOption option = UpdateOption.OnlyIfNotRecentlyUpdated);
+
+        void UpdateBrokerAccount(
+            IBroker broker,
+            IBrokersCandlesService candleService,
+            IMarketDetailsService marketsService,
+            ITradeDetailsAutoCalculatorService tradeCalculateService,
+            Action<string> updateProgressAction,
+            UpdateOption option = UpdateOption.OnlyIfNotRecentlyUpdated);
+
+        void RecalculateTrade(Trade trade, IBrokersCandlesService candleService, IMarketDetailsService marketsService, IBroker broker);
+    }
+}
