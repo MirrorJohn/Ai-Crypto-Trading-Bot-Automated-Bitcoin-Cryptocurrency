83749c709e7bea3ac0fa087cefdbb4df1964c480
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:19:11 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Indicators/AverageDayRange.cs
@@ -0,0 +1,35 @@
+﻿using TraderTools.Basics;
+
+namespace TraderTools.Indicators
+{
+    public class AverageDayRange : IIndicator
+    {
+        public AverageDayRange()
+        {
+            MovingAverage = new WilderMovingAverage();
+        }
+
+        public WilderMovingAverage MovingAverage { get; }
+
+        /// <summary>
+        /// Whether the indicator is set.
+        /// </summary>
+        public bool IsFormed => MovingAverage.IsFormed;
+
+        public void Reset()
+        {
+            MovingAverage.Reset();
+        }
+
+        public string Name => "ADR";
+
+        public SignalAndValue Process(Candle candle)
+        {
+            return MovingAverage.Process(new Candle
+            {
+                CloseBid = candle.HighAsk - candle.LowAsk,
+                IsComplete = candle.IsComplete
+            });
+        }
+    }
+}
