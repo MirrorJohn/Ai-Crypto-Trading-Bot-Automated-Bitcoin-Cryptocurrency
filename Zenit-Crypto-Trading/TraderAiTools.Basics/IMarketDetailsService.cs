8bce2442c0e0da1e9ea01859e812aeed6c624f31
CarmineCopc4v <odsfgy37cx@outlook.com>
2024-08-11 01:14:59 +0400

    Upload Files

---
+++ Zenit-Crypto-Trading/TraderAiTools.Basics/IMarketDetailsService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TraderTools.Basics
+{
+    public interface IMarketDetailsService
+    {
+        MarketDetails GetMarketDetails(string broker, string market);
+
+        List<MarketDetails> GetAllMarketDetails();
+
+        void AddMarketDetails(MarketDetails marketDetails);
+
+        bool HasMarketDetails(string broker, string market);
+
+        void SaveMarketDetailsList();
+    }
+}
